5a202949cb141c910e9ac24bdc89350f6b777991
sebastian-kopiczko <sebastian.kopiczko@gmail.com>
2017-03-05 18:06:18 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+gulp.task('sass', function () {
+    return gulp.src('app/scss/styles.scss').pipe(sass().on('error', sass.logError)).pipe(autoprefixer()).pipe(gulp.dest('app/css')).pipe(browserSync.reload({
+        stream: true
+    }));
+});
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: 'app'
+        }
+    , })
+})
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/*.html', browserSync.reload);
+});
