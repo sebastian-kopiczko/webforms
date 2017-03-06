c77c6b5b87d5be9123dd3c8b685ef9a811012989
sebastian-kopiczko <sebastian.kopiczko@gmail.com>
2017-03-07 00:56:39 +0100

    webforms html + css first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 gulp.task('sass', function () {
     return gulp.src('app/scss/styles.scss').pipe(sass().on('error', sass.logError)).pipe(autoprefixer()).pipe(gulp.dest('app/css')).pipe(browserSync.reload({
@@ -17,4 +18,7 @@ gulp.task('browserSync', function () {
 gulp.task('watch', ['browserSync', 'sass'], function () {
     gulp.watch('app/scss/**/*.scss', ['sass']);
     gulp.watch('app/*.html', browserSync.reload);
+});
+gulp.task('imagemin', function () {
+    gulp.src('app/images/*').pipe(imagemin()).pipe(gulp.dest('app/img'))
 });
